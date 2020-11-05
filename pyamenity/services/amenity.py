4e06e3f638f72a46b3d9a922e9b8aa882345e7f8
Matthew Grossman <matthewryangrossman@gmail.com>
2020-11-05 11:13:59 -0800

    format

--- pyamenity/services/amenity.py
+++ pyamenity/services/amenity.py
@@ -1,22 +1,29 @@
 from datetime import datetime
 from datetime import timezone
 from typing import Any
+from typing import Dict
 
 import requests
 
 
 class Amenity:
-    URL_STATISTICS = "https://d1va2v45wuqutp.cloudfront.net/v1/permits/policies/issue/statistics"
+    URL_STATISTICS = (
+        "https://d1va2v45wuqutp.cloudfront.net/v1/permits/policies/issue/statistics"
+    )
 
     def __init__(self, scope: str):
         self._client = requests.Session()
         self._scope = scope
 
-    def _get_statistics(self) -> dict[str, Any]:
+    def _get_statistics(self) -> Dict[str, Any]:
         now = datetime.now(timezone.utc)
         valid = now.date()
-        self._client.get(self.URL_STATISTICS, params={
-            'scope': self._scope,
-            'viewpoint': viewpoint.isoformat(),
-            'valid': valid.isoformat(),
-        })
+        resp = self._client.get(
+            self.URL_STATISTICS,
+            params={
+                "scope": self._scope,
+                "viewpoint": valid.isoformat(),
+                "valid": valid.isoformat(),
+            },
+        )
+        return resp.json()
